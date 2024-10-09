a45fccaf07637ef6aa5b4652b8c5d9aa21e15d16
InesParis <inesparisarranz@gmail.com>
2024-10-09 14:57:56 +0200

    working on navbar responsiveness
    js file to work on the navbar

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.getElementById("toggle-button");
+  const navbarLinks = document.getElementById("navbar-links");
+
+  toggleButton.addEventListener("click", function () {
+    navbarLinks.classList.toggle("active");
+  });
+});
